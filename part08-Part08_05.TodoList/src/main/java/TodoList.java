910f9f8b07f13f8378048d97eccd39c2da48bbbe
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-29 10:03:16 +0100

    Add files via upload

---
+++ part08-Part08_05.TodoList/src/main/java/TodoList.java
@@ -0,0 +1,41 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ania
+ */
+public class TodoList {
+    
+    private ArrayList <String> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<>();
+    }
+    
+    public void add (String task) {
+        this.tasks.add(task);
+    }
+    
+    public void print () {
+        for(int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ": " + tasks.get(i));
+        }
+    }
+    
+    public void remove (int number) {
+//        for(int i = 0; i < tasks.size(); i ++) {
+            tasks.remove(number - 1);
+//        }
+        
+    }
+    
+    
+}
