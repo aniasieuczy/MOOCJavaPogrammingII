ebd40b7fd96ac98033da28ca35b2db5b25e26c3c
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-30 15:28:25 +0100

    Add files via upload

---
+++ part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Set<String> names = new HashSet<>();
+        names.add("first");
+        names.add("first");
+        names.add("second");
+        names.add("second");
+        names.add("second");
+
+System.out.println(returnSize(names));
+
+    }
+    
+    public static int returnSize (Set set) {
+//        int i = 0;
+//        for(Object element: set) {
+//            i++;
+//            
+//            if(element.equals(element)) {    
+//            }
+//        }
+//        return i;
+//    }
+        
+        return set.size();
+    }
+  
+
+    // implement the method returnSize here, which returns
+    // the number of elements in the set that it receives as a parameter.
+
+}
