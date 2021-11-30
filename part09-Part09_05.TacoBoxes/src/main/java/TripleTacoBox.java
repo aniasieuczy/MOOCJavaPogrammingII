e9bf61995a569cc5a7663b2945ca001eed142b37
Anna Szpecht <63251402+aniasieuczy@users.noreply.github.com>
2021-11-30 15:26:48 +0100

    Add files via upload

---
+++ part09-Part09_05.TacoBoxes/src/main/java/TripleTacoBox.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ania
+ */
+public class TripleTacoBox implements TacoBox {
+
+    private int tacos;
+
+    public TripleTacoBox() {
+        this.tacos = 3;
+    }
+
+    public int tacosRemaining() {
+    return this.tacos;
+    } 
+
+    public void eat () {
+    this.tacos = this.tacos - 1;
+        if(this.tacos <= 0) {
+            this.tacos = 0;
+        }
+    }
+
+}
